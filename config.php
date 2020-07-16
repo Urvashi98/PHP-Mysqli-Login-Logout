236dd000e976298571e02ce8ff731905de5b9548
Urvashi Gajjar <64082203+Urvashi98@users.noreply.github.com>
2020-07-16 15:25:15 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+
+$conn = mysqli_connect('localhost','root','','users');
+if(!$conn){
+    echo 'connection failed  ' . mysqli_connect_error();
+}else{
+
+    
+  //  echo 'Connection OK';
+}
+?>
---
+++ index.php
@@ -0,0 +1,40 @@
+
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <title>Index</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+       
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+         <!-- fFont Awesome CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    </head>
+      <body>
+          
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="jumbotron">
+                        <h1 class="display-3"><i class="fa fa-home  " aria-hidden="true"></i>WELCOME!</h1>
+                        <p class="lead">This is a tutorial for Login-Logout functionality using <b>PHP & MySqli</b></p>
+                        <hr class="my-5">
+                        <p>To render the Profile, You need login first!!</p>
+                        <p class="lead">
+                            <a class="btn btn-primary btn-lg" href="login.php" role="button">Login</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+       
+      </body>
+    </html>
